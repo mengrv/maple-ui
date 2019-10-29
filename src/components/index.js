405d141f7cf0ad0d7f8398d6d8d33df550d7a036
孟瑞 <mr@marsgis.cn>
2019-10-29 15:07:23 +0800

    暴露Tree组件

--- src/components/index.js
+++ src/components/index.js
@@ -3,25 +3,26 @@ import '@/style/flex.less';
 import '@/style/scroll.less';
 import '../util/util.js';
 import Error404 from './Error-404';
-import Icon     from "./Icon";
-import Picture  from "./Picture";
-import Spin     from "./Spin";
+import Icon from "./Icon";
+import Picture from "./Picture";
+import Spin from "./Spin";
+import Tree from "./Tree/Tree";
 
-const components = {Error404, Icon, Picture, Spin};
+const components = {Error404, Icon, Picture, Spin, Tree};
 
 const Plugin = {};
 
-Plugin.install = function (Vue) {
-  Object.keys(components).map(key => {
-    Vue.component(`maple${components[key].name}`, components[key]);
-  });
+Plugin.install = function(Vue) {
+	Object.keys(components).map(key => {
+		Vue.component(`maple${components[key].name}`, components[key]);
+	});
 };
 
 
-if (typeof window !== 'undefined' && window.Vue) {
-  Plugin.install(window.Vue);
+if(typeof window !== 'undefined' && window.Vue) {
+	Plugin.install(window.Vue);
 }
 
 
-export { Error404, Icon, Picture, Spin };
+export { Error404, Icon, Picture, Spin, Tree };
 export default Plugin;
